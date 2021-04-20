13174167906584df72245216addcd705afba3c49
sabdadev <jasonalhilal@gmail.com>
2021-04-20 08:18:27 +0700

    JS Data Visualization

---
+++ basic/script.js
@@ -0,0 +1,15 @@
+ // Gen random data
+ const N = 300;
+ const gData = [...Array(N).keys()].map(() => ({
+     lat: (Math.random() - 0.5) * 180,
+     lng: (Math.random() - 0.5) * 360,
+     size: Math.random() / 3,
+     color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+ }));
+
+ Globe()
+     .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+     .pointsData(gData)
+     .pointAltitude('size')
+     .pointColor('color')
+     (document.getElementById('globeViz'))
